4e8c37fe64dedd6b1d1a726af4ca07053f560051
kholisyah <kholisyah23@mhs.politala.ac.id>
2024-10-22 12:50:42 +0800

    memperbaiki sedikit kode

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,14 @@ Route::get('/dasbord', function () {
 // Rute untuk 'jadwal'
 Route::get('/jadwal', [AkunController::class, 'index']);
 
+//Rout pendaftaran
 // Route untuk menampilkan daftar post
 Route::get('/project', [PostController::class, 'index'])->name('posts.index');
-
 // Route untuk create post
 Route::post('/create-post', [PostController::class, 'createPost'])->name('create-post');
-
 //Route untuk edit post
 Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
 Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
-
 //Route untuk delete post
 Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
 
