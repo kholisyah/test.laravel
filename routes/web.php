ec1c0863f64554f03d03d79396ce765833e6da8a
na <nadellaisasyarifa23@mhs.politala.ac.id>
2024-11-05 08:28:53 +0700

    delete tabel yang tidak berfungsi

--- routes/web.php
+++ routes/web.php
@@ -68,51 +68,6 @@ Route::get('/edit-posts/{id}', [AkunController::class, 'edit'])->name('edit');
 Route::post('/update-posts/{id}', [AkunController::class, 'update'])->name('update');
 
 
-// Barang Routes
-Route::resource('barang', BarangController::class);
-Route::get('barang/{barang}/edit', [BarangController::class, 'edit'])->name('barang.edit');
-Route::post('barang/store', [BarangController::class, 'store'])->name('barang.store');
-Route::put('barang/{barang}', [BarangController::class, 'update'])->name('barang.update');
-Route::delete('barang/{barang}', [BarangController::class, 'destroy'])->name('barang.destroy');
-// Route untuk form edit
-Route::get('/barang/edit/{id_barang}', [BarangController::class, 'edit']);
-
-// Route untuk proses update
-Route::put('/barang/update/{id_barang}', [BarangController::class, 'update']);
-// route untuk proses delete
-Route::delete('/barang/delete/{id_barang}', [BarangController::class, 'destroy'])->name('barang.destroy');
-// Route untuk menampilkan form edit
-Route::get('/barang/edit/{id_barang}', [BarangController::class, 'edit'])->name('barang.edit');
-
-// Route untuk memproses update barang
-Route::put('/barang/update/{id_barang}', [BarangController::class, 'update'])->name('barang.update');
-// Rute untuk menampilkan form edit
-Route::get('/barang/edit/{id}', [BarangController::class, 'edit'])->name('barang.edit');
-
-// Rute untuk memproses update barang
-Route::put('/barang/update/{id}', [BarangController::class, 'update'])->name('barang.update');
-
-// untuk pelanggan
-
-Route::put('/pelanggan/update/{id}', [PelangganController::class, 'update'])->name('pelanggan.update');
-
-// Rute untuk menampilkan daftar pelanggan
-Route::get('/pelanggan', [PelangganController::class, 'index'])->name('pelanggan.index');
-
-// Rute untuk menyimpan pelanggan baru
-Route::post('/pelanggan/store', [PelangganController::class, 'store'])->name('pelanggan.store');
-
-// Rute untuk mengedit pelanggan
-Route::get('/pelanggan/edit/{id}', [PelangganController::class, 'edit'])->name('pelanggan.edit');
-Route::post('/pelanggan/update/{id}', [PelangganController::class, 'update'])->name('pelanggan.update');
-
-// Rute untuk menghapus pelanggan
-Route::delete('/pelanggan/destroy/{id}', [PelangganController::class, 'destroy'])->name('pelanggan.destroy');
-
-Route::put('/pelanggan/update/{id}', [PelangganController::class, 'update']);
-Route::get('/index', [PelangganController::class, 'index'])->name('index');
-
-
 // Rute untuk fitur penyewaan
 
 // Menampilkan daftar penyewaan
@@ -142,47 +97,6 @@ Route::delete('/penyewaan/{id}', [PenyewaanController::class, 'destroy'])->name(
 // Menampilkan detail penyewaan berdasarkan ID
 Route::get('/penyewaan/{id}', [PenyewaanController::class, 'show'])->name('penyewaan.show');
 // Penjelasan: Rute ini digunakan untuk menampilkan detail lengkap penyewaan tertentu. Metode `show` pada `PenyewaanController` akan menampilkan informasi rinci tentang penyewaan yang dipilih berdasarkan ID yang diberikan di URL (`{id}`).
-
-//admin
-Route::resource('admin', AdminController::class);
-
-// Menampilkan daftar admin
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-
-// Menyimpan admin baru
-Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
-
-// Menampilkan form edit admin
-Route::get('/admin/edit/{admin}', [AdminController::class, 'edit'])->name('admin.edit');
-
-// Memperbarui admin
-Route::put('/admin/{admin}', [AdminController::class, 'update'])->name('admin.update');
-
-// Menghapus admin
-Route::delete('/admin/{admin}', [AdminController::class, 'destroy'])->name('admin.destroy');
-
-// Route untuk semua operasi terkait transaksi
-
-// Menampilkan daftar semua transaksi
-Route::get('/transaksi', [TransaksiController::class, 'index'])->name('transaksi.index');
-
-// Menampilkan form pembuatan transaksi baru
-Route::get('/transaksi/create', [TransaksiController::class, 'create'])->name('transaksi.create');
-
-// Menyimpan data transaksi baru ke dalam database
-Route::post('/transaksi', [TransaksiController::class, 'store'])->name('transaksi.store');
-
-// Menampilkan form edit untuk transaksi berdasarkan ID
-Route::get('/transaksi/{id}/edit', [TransaksiController::class, 'edit'])->name('transaksi.edit');
-
-// Memperbarui data transaksi yang sudah ada di database
-Route::put('/transaksi/{id}', [TransaksiController::class, 'update'])->name('transaksi.update');
-
-// Menghapus transaksi berdasarkan ID dari database
-Route::delete('/transaksi/{id}', [TransaksiController::class, 'destroy'])->name('transaksi.destroy');
-
-// Menampilkan detail dari transaksi tertentu berdasarkan ID
-Route::get('/transaksi/{id}', [TransaksiController::class, 'show'])->name('transaksi.show');
 
 
 // Rute untuk melihat jadwal latihan
