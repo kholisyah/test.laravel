224af0f6c5668903776480d433e8b391821ba4ed
suchiaprilia <suchiaprilia23@mhs.politala.ac.id>
2024-11-20 12:22:32 +0700

    add: user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $table = 'User'; 
-    protected $fillable = [
-        'nama',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'email',
-        'role',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'password' => 'hashed',
-        ];
-    }
+    protected $table = 'user'; // Pastikan tabel sesuai dengan migrasi
+    protected $fillable = ['nama', 'password', 'email'];
 }
